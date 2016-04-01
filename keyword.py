91f769b252ec2f9d18ab09c40bda892089dfaabc
魚跃 <pisces.hj@gmail.com>
2016-04-01 08:45:53 +0800

    modify

---
+++ keyword.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+sum = 0
+n = 100
+while n > 0:
+    sum = sum + n
+    n = n - 1
+print(sum)
+
+L = ['Bart', 'Lisa', 'Adam']
+for name in L:
+    print('Hello,', name,'!')
+
+i = 0
+while i<100:
+    i=i+1
+print(i)
